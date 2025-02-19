fbd812e2bea2a0864b9007df4bff17afa432602a
tolmachdr <tolmachevadarya971@gmail.com>
2025-02-19 22:26:50 +0300

    feat(metrics): python app

--- app_python/main.py
+++ app_python/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from datetime import datetime
 import pytz
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+from starlette.responses import Response
+
 
 app = FastAPI()
 
@@ -10,3 +13,8 @@ def get_time():
     timezone = pytz.timezone("Europe/Moscow")
     time = datetime.now(timezone)
     return {"moscow_time": time}
+
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
