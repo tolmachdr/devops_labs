d7806adfcd5269e3d8d6e34d44e6378e48408e29
dasha <dasha@LAPTOP-382BEF3K>
2025-03-06 21:23:30 +0300

    feat(lab12)

--- app_python/main.py
+++ app_python/main.py
@@ -1,15 +1,41 @@
 from fastapi import FastAPI
+import os
 from datetime import datetime
 import pytz
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from starlette.responses import Response
 
-
+VISITS_FILE = "/app/data/visits.txt"
 app = FastAPI()
+
+
+def initialize_visits_file():
+    os.makedirs(os.path.dirname(VISITS_FILE), exist_ok=True)
+    if not os.path.exists(VISITS_FILE):
+        with open(VISITS_FILE, "w") as f:
+            f.write("0")
+
+
+initialize_visits_file()
+
+
+def get_visits():
+    if os.path.exists(VISITS_FILE):
+        with open(VISITS_FILE, "r") as f:
+            return int(f.read().strip())
+    return 0
+
+
+def increment_visits():
+    visits = get_visits() + 1
+    with open(VISITS_FILE, "w") as f:
+        f.write(str(visits))
+    return visits
 
 
 @app.get("/")
 def get_time():
+    visits = increment_visits()
     timezone = pytz.timezone("Europe/Moscow")
     time = datetime.now(timezone)
     return {"moscow_time": time}
@@ -18,3 +44,8 @@ def get_time():
 @app.get("/metrics")
 def metrics():
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
+
+@app.get("/visits")
+def get_visits_count():
+    return {"visits": get_visits()}
